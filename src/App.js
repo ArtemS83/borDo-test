da13793ccdc6222e222e8ecc30df4277575c542e
Artem Sergieiev <66088984+ArtemS83@users.noreply.github.com>
2021-08-30 23:01:54 +0200

    start

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Table from './components/Table';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello React!</h1>
+      <Table />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Table/Table.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Table.module.css';
+
+function Table() {
+  return (
+    <div>
+      <h2>Hello Table!</h2>
+    </div>
+  );
+}
+
+export default Table;
