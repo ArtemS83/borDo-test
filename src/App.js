18d3bcb858bf48446d2236c2b9ff9e4f14095d72
Artem Sergieiev <66088984+ArtemS83@users.noreply.github.com>
2021-08-31 17:44:18 +0200

    add inifinity scroll

--- src/App.js
+++ src/App.js
@@ -4,19 +4,45 @@ import newsApi from './services/newsApi';
 
 function App() {
   const [news, setNews] = useState([]);
+  const [loadMore, setLoadMore] = useState(false);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
-    newsApi
-      .fetchNews()
-      .then(data => {
-        setNews(data);
-      })
-      .catch(error => console.log('ERROR: ', error));
+    newsApi.fetchNews(page).then(data => {
+      setNews([...news, ...data]);
+    });
   }, []);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  useEffect(() => {
+    if (loadMore && page <= 10) {
+      newsApi.fetchNews(page).then(data => {
+        setNews([...news, ...data]);
+      });
+    }
+    setLoadMore(false);
+  }, [loadMore]);
+
+  function handleScroll() {
+    if (
+      window.innerHeight + document.documentElement.scrollTop !==
+      document.documentElement.offsetHeight
+    )
+      return;
+    setPage(prevPage => prevPage + 1);
+    setLoadMore(true);
+  }
 
   return (
     <>
       <Table news={news} />
+      {loadMore && (
+        <div style={{ textAlign: 'center', fontSize: '24px' }}>Loding..</div>
+      )}
     </>
   );
 }
