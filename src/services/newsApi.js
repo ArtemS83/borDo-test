824c68fde006251ace0b2b009919442ea0091a88
Artem Sergieiev <66088984+ArtemS83@users.noreply.github.com>
2021-09-03 18:13:53 +0200

    add axios

--- src/services/newsApi.js
+++ src/services/newsApi.js
@@ -1,54 +1,85 @@
-const BASE_URL = 'https://api.hnpwa.com/v0/';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://api.hnpwa.com/v0/';
 
 const fetchNews = (currentPage = 1) => {
-  return fetch(`${BASE_URL}news/${currentPage}.json`, {
-    method: 'GET',
-    headers: {
-      Accept: 'application/json',
-      mode: 'no-cors',
-    },
-  })
-    .then(response => {
-      if (response.ok) return response.json();
-      throw new Error('Error fetching data');
-    })
-    .catch(err => {
-      console.error('Error: ', err);
-    });
-};
-
-const fetchComments = id => {
-  return fetch(`${BASE_URL}item/${id}.json`, {
-    method: 'GET',
-    headers: {
-      Accept: 'application/json',
-      mode: 'no-cors',
-    },
-  })
-    .then(response => {
-      if (response.ok) return response.json();
-      throw new Error('Error fetching data');
-    })
+  return axios
+    .get(`news/${currentPage}.json`)
+    .then(({ data }) => data)
     .catch(err => {
       console.error('Error: ', err);
     });
 };
 
 const fetchNewest = (currentPage = 1) => {
-  return fetch(`${BASE_URL}newest/${currentPage}.json`, {
-    method: 'GET',
-    headers: {
-      Accept: 'application/json',
-      mode: 'no-cors',
-    },
-  })
-    .then(response => {
-      if (response.ok) return response.json();
-      throw new Error('Error fetching data');
-    })
+  return axios
+    .get(`newest/${currentPage}.json`)
+    .then(({ data }) => data)
     .catch(err => {
       console.error('Error: ', err);
     });
 };
+
+const fetchComments = id => {
+  return axios
+    .get(`item/${id}.json`)
+    .then(({ data }) => data)
+    .catch(err => {
+      console.error('Error: ', err);
+    });
+};
+
+// const BASE_URL = 'https://api.hnpwa.com/v0/';
+
+// const fetchNews = (currentPage = 1) => {
+//   return fetch(`${BASE_URL}news/${currentPage}.json`, {
+//     method: 'GET',
+//     headers: {
+//       Accept: 'application/json',
+//       mode: 'no-cors',
+//     },
+//   })
+//     .then(response => {
+//       if (response.ok) return response.json();
+//       throw new Error('Error fetching data');
+//     })
+//     .catch(err => {
+//       console.error('Error: ', err);
+//     });
+// };
+
+// const fetchComments = id => {
+//   return fetch(`${BASE_URL}item/${id}.json`, {
+//     method: 'GET',
+//     headers: {
+//       Accept: 'application/json',
+//       mode: 'no-cors',
+//     },
+//   })
+//     .then(response => {
+//       if (response.ok) return response.json();
+//       throw new Error('Error fetching data');
+//     })
+//     .catch(err => {
+//       console.error('Error: ', err);
+//     });
+// };
+
+// const fetchNewest = (currentPage = 1) => {
+//   return fetch(`${BASE_URL}newest/${currentPage}.json`, {
+//     method: 'GET',
+//     headers: {
+//       Accept: 'application/json',
+//       mode: 'no-cors',
+//     },
+//   })
+//     .then(response => {
+//       if (response.ok) return response.json();
+//       throw new Error('Error fetching data');
+//     })
+//     .catch(err => {
+//       console.error('Error: ', err);
+//     });
+// };
 
 export default { fetchNews, fetchComments, fetchNewest };
